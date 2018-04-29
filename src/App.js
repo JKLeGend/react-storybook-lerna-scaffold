93f0a2805ab32b9bed2f564fb7c9a0d913dbff19
jukai.zhang <jukai.zhang@rea-group.com>
2018-04-29 14:07:00 +0800

    install published statefull and stateless button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StatefullReactButton from 'statefull-react-button';
+import StatelessReactButton from 'stateless-react-button';
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <StatefullReactButton handleOnclick={() => {alert('haha')}} />
+        <StatelessReactButton handleOnclick={() => {alert('hehe')}} />
       </div>
     );
   }
